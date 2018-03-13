07e55acc8471744302ad65ead2657938b813f962
iman1991 <iman1991@mail.ru>
2018-03-13 14:10:38 +0300

    added structure for drainage

--- Code/inc/drainage.h
+++ Code/inc/drainage.h
@@ -1,3 +1,9 @@
 #include "stdint.h"
 
-uint8_t drainage_control(uint8_t sensor_1, uint8_t sensor_2);
+typedef struct Drainage {
+  uint32_t time;
+  uint8_t pump;
+}Drainage;
+
+
+uint8_t drainage_control(Drainage *drainage, uint8_t sensor_1, uint8_t sensor_2);
